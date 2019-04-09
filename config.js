c8acceb5988461d5498652d8c9fafb2231d64690
Łukasz Król <lionel@o2.pl>
2019-04-09 18:46:27 +0200

    Add module with API config

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  "GOOGLE_CLIENT_ID": "3519180928-ukv1v0s25rctr3ppp3ch589jaqnmosov.apps.googleusercontent.com",
+  "GOOGLE_CLIENT_SECRET": "tvLVMvb9caTrca60xSsd3Bz6",
+  "CALLBACK_URL": "http://localhost:3000/auth/google/callback"
+};
